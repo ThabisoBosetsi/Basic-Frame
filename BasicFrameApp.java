4e57afc5dd627f34a56e6829488b20e1be47c90c
ThabisoBosetsi <140021153+ThabisoBosetsi@users.noreply.github.com>
2023-07-19 23:43:25 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,21 @@
+package basicframeapp;
+
+import za.ac.tut.MyFirstFrame;
+
+/**
+ *
+ * @author Admin
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    MyFirstFrame myFrame;
+    
+    myFrame=new MyFirstFrame();
+    }
+    
+}
---
+++ MyFirstFrame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFirstFrame extends JFrame{
+    public MyFirstFrame(){
+        setTitle("My First GUI");
+        setSize(600,650);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+    }
+}
